09ad96f9530c6d29065c6e3f685e16bc8058c239
Joozek3000 <j.jozefczyk90@gmail.com>
2023-06-05 17:22:08 +0100

    ui change, added comments.

--- src/ui.js
+++ src/ui.js
@@ -1,5 +1,5 @@
 import { format } from 'date-fns';
-import Todo from './todo';
+import './styles.css';
 
 let project = null;
 
@@ -56,14 +56,14 @@ function renderTodo(todo, todoList) {
   return todoElement;
 }
 
-// Render a list of todos
+/// Render a list of todos
 function renderTodoList(todoList) {
-  const todoListElement = createElement('div', ['todo-list']);
+  const todoContainer = createElement('div', ['todo-container']); // Create a container for todos
   todoList.getAllTodos().forEach((todo) => {
     const todoElement = renderTodo(todo, todoList);
-    todoListElement.appendChild(todoElement);
+    todoContainer.appendChild(todoElement); // Append each todo element to the container
   });
-  return todoListElement;
+  return todoContainer; // Return the container
 }
 
 // Render a new todo form
@@ -73,14 +73,30 @@ function renderNewTodoForm(todoList, project) {
     return;
   }
 
-  const form = createElement('form', ['new-todo-form']);
-  const titleInput = createElement('input', ['title-input']);
-  const dueDateInput = createElement('input', ['due-date-input']);
-  const priorityInput = createElement('select', ['priority-input']);
-  const priorities = ['Low', 'Medium', 'High'];
-  const detailsInput = createElement('input', ['details-input']);
-  const submitButton = createElement('button', ['submit-button']);
+  const form = createElement('form', ['new-todo-form']); // Create a form element with 'new-todo-form' class
+  const titleInput = createElement('input', ['title-input']); // Create an input element for the title with 'title-input' class
+  const dueDateInput = createElement('input', ['due-date-input']); // Create an input element for the due date with 'due-date-input' class
+  const priorityInput = createElement('select', ['priority-input']); // Create a select element for the priority with 'priority-input' class
+  const priorities = ['Low', 'Medium', 'High']; // Define the available priority options
+  const detailsInput = createElement('input', ['details-input']); // Create an input element for the details with 'details-input' class
+  const submitButton = createElement('button', ['submit-button']); // Create a button element for submitting the form with 'submit-button' class
 
+  // Create modal and modal content
+  const modal = createElement('div', ['modal']); // Create a div element for the modal with 'modal' class
+  const modalContent = createElement('div', ['modal-content']); // Create a div element for the modal content with 'modal-content' class
+
+  // Append the form to the modal content
+  modalContent.appendChild(form);
+  modal.appendChild(modalContent);
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = 'none';
+    }
+  };
+
+  // Create option elements for each priority and append them to the priority select element
   priorities.forEach((priority) => {
     const option = document.createElement('option');
     option.value = priority;
@@ -88,55 +104,73 @@ function renderNewTodoForm(todoList, project) {
     priorityInput.appendChild(option);
   });
 
-  titleInput.placeholder = 'Title';
-  dueDateInput.placeholder = 'Due date';
-  priorityInput.placeholder = 'Priority';
-  detailsInput.placeholder = 'Details';
-  submitButton.textContent = 'Add todo';
+  titleInput.placeholder = 'Title'; // Set the placeholder text for the title input
+  dueDateInput.placeholder = 'Due date'; // Set the placeholder text for the due date input
+  priorityInput.placeholder = 'Priority'; // Set the placeholder text for the priority select element
+  detailsInput.placeholder = 'Details'; // Set the placeholder text for the details input
+  submitButton.textContent = 'Add todo'; // Set the text content of the submit button
 
-  submitButton.type = 'submit';
-  dueDateInput.type = 'date';
+  submitButton.type = 'submit'; // Set the type of the submit button to 'submit'
+  dueDateInput.type = 'date'; // Set the type of the due date input to 'date'
 
-  form.appendChild(titleInput);
-  form.appendChild(dueDateInput);
-  form.appendChild(priorityInput);
-  form.appendChild(detailsInput);
-  form.appendChild(submitButton);
+  form.appendChild(titleInput); // Append the title input to the form
+  form.appendChild(dueDateInput); // Append the due date input to the form
+  form.appendChild(priorityInput); // Append the priority select element to the form
+  form.appendChild(detailsInput); // Append the details input to the form
+  form.appendChild(submitButton); // Append the submit button to the form
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const title = titleInput.value;
-    const dueDate = dueDateInput.value;
-    const priority = priorityInput.value;
-    const description = detailsInput.value;
+    const title = titleInput.value; // Get the value of the title input
+    const dueDate = dueDateInput.value; // Get the value of the due date input
+    const priority = priorityInput.value; // Get the value of the priority select element
+    const description = detailsInput.value; // Get the value of the details input
 
     if (
-      !title.trim() ||
-      !dueDate.trim() ||
-      !priority.trim() ||
-      !description.trim()
+      !title.trim() || // Check if the title is empty or contains only whitespace
+      !dueDate.trim() || // Check if the due date is empty or contains only whitespace
+      !priority.trim() || // Check if the priority is empty or contains only whitespace
+      !description.trim() // Check if the description is empty or contains only whitespace
     ) {
-      alert('Please fill out all fields');
+      alert('Please fill out all fields'); // Display an alert message if any field is empty
       return;
     }
 
-    todoList.addTodo(title, description, new Date(dueDate), priority);
-    renderProject(project);
+    todoList.addTodo(title, description, new Date(dueDate), priority); // Add the new todo to the todo list
+    renderProject(project); // Render the updated project
   });
 
-  return form;
+  return modal; // Return the modal element
+}
+
+// Render an add button
+function renderAddButton() {
+  const addButton = createElement('button', ['add-button']);
+  addButton.textContent = '+';
+  addButton.addEventListener('click', () => {
+    // Find the modal and display it when the add button is clicked
+    const modal = document.querySelector('.modal');
+    if (modal) {
+      modal.style.display = 'block';
+    }
+  });
+  return addButton;
 }
 
 // Render a project
 export function renderProject(project) {
   root.innerHTML = ''; // Clear the root element
 
-  // Render each todolist in the project
+  // Render the add button and append it to the root
+  const addButton = renderAddButton();
+  root.appendChild(addButton);
+
+  // Render each todo list in the project
   project.getAllTodoLists().forEach((todoList) => {
     const todoListElement = renderTodoList(todoList);
-    root.appendChild(todoListElement);
+    root.appendChild(todoListElement); // Append each todo list element to the root
 
     const newTodoForm = renderNewTodoForm(todoList, project);
-    root.appendChild(newTodoForm);
+    root.appendChild(newTodoForm); // Append each new todo form to the root
   });
 }
